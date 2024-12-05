fc523e36107cc8bb8af7e1aec799292708554caa
ali-raza-baig <alirazabaigmdk@gmail.com>
2024-12-05 18:02:26 +0500

    Packgas setup

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv"
+
+// Configraturation Settings
 const app = express();
+dotenv.config();
 
 
+// Middleware configraturation
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.use(cookieParser());
+
+
+// App initialization
 app.listen(5000, () => {
   console.log("App listing on 5000");
 });
